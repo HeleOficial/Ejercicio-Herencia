4625ccd0ae684699ba9cd58db806561ed4d4ebaf
Tatiana Urbano <turbano@estudiante.uniajc.edu.co>
2025-03-04 20:26:53 -0500

    Ejemploo uso de git en neatbeans

--- src/ejercicio/herencia/EjercicioHerencia.java
+++ src/ejercicio/herencia/EjercicioHerencia.java
@@ -16,7 +16,7 @@ public class EjercicioHerencia {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo 2");
         // TODO code application logic here
     }
     
